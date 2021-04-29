c7467bb285c716ebe11c802ef470b122df1774a6
ryan10599 <thanapol.ryan@mail.kmutt.ac.th>
2021-04-29 15:47:02 +0700

    [ADD] add component upload image

---
+++ src/components/UploadImage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="choose-file-box">
+      <!-- Styled -->
+      <b-form-file
+        v-model="file1"
+        :state="Boolean(file1)"
+        placeholder="Choose a file or drop it here..."
+        drop-placeholder="Drop file here..."
+      ></b-form-file>
+      <br />
+      <div class="mt-3">Selected file: {{ file1 ? file1.name : "" }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      file1: null,
+    };
+  },
+};
+</script>
+
+<style>
+.choose-file-box{
+    padding: 10px;
+    margin-left: 20%;
+    margin-right: 20%;
+    border: 2px dashed;
+}
+</style>

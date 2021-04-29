261621b538054f98fd60cc200a85008aaa5609d8
ryan10599 <thanapol.ryan@mail.kmutt.ac.th>
2021-04-29 16:15:12 +0700

    [ADD] add bootstrap select file

--- src/components/UploadImage.vue
+++ src/components/UploadImage.vue
@@ -4,12 +4,21 @@
       <!-- Styled -->
       <b-form-file
         v-model="file1"
+        @change="onFileSelected"
         :state="Boolean(file1)"
         placeholder="Choose a file or drop it here..."
         drop-placeholder="Drop file here..."
       ></b-form-file>
+      <b-button :disabled="this.url != null ? false : true">Upload</b-button>
       <br />
-      <div class="mt-3">Selected file: {{ file1 ? file1.name : "" }}</div>
+      <br />
+      <div>
+        <h3>Preview Image</h3>
+        <div class="preview-image">
+          <img :src="url" alt="" />
+        </div>
+      </div>
+      <!-- <div class="mt-3">Selected file: {{ file1 ? file1.name : "" }}</div> -->
     </div>
   </div>
 </template>
@@ -19,16 +28,32 @@ export default {
   data() {
     return {
       file1: null,
+      selectedFile: null,
+      msg: "test",
+      url: null,
     };
+  },
+  methods: {
+    onFileSelected(event) {
+      console.log(event);
+      console.log("hi");
+      this.selectedFile = event.target.files[0];
+      console.log(this.selectedFile);
+      this.url = URL.createObjectURL(this.selectedFile);
+      console.log(this.url);
+    },
   },
 };
 </script>
 
 <style>
-.choose-file-box{
-    padding: 10px;
-    margin-left: 20%;
-    margin-right: 20%;
-    border: 2px dashed;
+.choose-file-box {
+  padding: 10px;
+  margin-left: 20%;
+  margin-right: 20%;
+  border: 2px dashed;
+}
+.preview-image img {
+  width: 100%;
 }
 </style>

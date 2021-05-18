1f0b6840c1ec9f7bbf69d0328b6370b39bb9e3f0
Jirapan Yankhan <jirapan_yankhan@hotmail.com>
2021-05-18 14:42:25 +0700

    [EDIT] Clean project

---
+++ src/components/Welcome.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>Hi Welcome to Pokemon </h2>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+export default class Welcome extends Vue {}
+</script>

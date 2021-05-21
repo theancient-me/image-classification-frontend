a470127fd5a5354e2430112327ad53c6f6876378
Jirapan Yankhan <jirapan_yankhan@hotmail.com>
2021-05-21 16:46:12 +0700

    [EDIT] Loading before into  pokendex

--- src/views/Loading.vue
+++ src/views/Loading.vue
@@ -1,11 +1,54 @@
 <template>
-    <div>
-        Loading
-    </div>
+  <div>
+    <h4>{{ countDown }}</h4>
+    <PokemonLoading></PokemonLoading>
+  </div>
 </template>
 
 <script>
+import PokemonLoading from "@/components/PokemonLoading";
+import pokeySound from "../assets/sound/anvilletown.mp3";
+import { Howl, Howler } from "howler";
+
 export default {
-    name : "LoadingScreen"
-}
+  name: "LoadingScreen",
+  components: {
+    PokemonLoading,
+  },
+  data() {
+    return {
+      countDown: 10,
+    };
+  },
+  mounted() {
+    this.playingSound();
+  },
+
+  created() {
+    this.countDownTimer();
+  },
+
+  methods: {
+    playingSound() {
+      var sound = new Howl({
+        src: [pokeySound],
+         volume: 0.5,
+      });
+
+      sound.play();
+    },
+    countDownTimer() {
+      if (this.countDown > 0) {
+        setTimeout(() => {
+          this.countDown -= 1;
+          this.countDownTimer();
+        }, 1000);
+      }else{
+          this.$router.push({
+              name : "Pokedex"
+          })
+      }
+    },
+  },
+};
 </script>
